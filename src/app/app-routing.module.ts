6c583f2b7362640a278778f7d9dec4ab47f216d9
mornaeldernar <jimenez.rafa@gmail.com>
2023-02-16 11:38:48 -0600

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,17 +9,18 @@ import { PacienteListComponent } from './modulos/paciente/components/paciente.li
 import { PacienteViewComponent } from './modulos/paciente/components/paciente.view/paciente.view.component';
 import { DiagnosticoFormComponent } from './modulos/diagnostico/components/diagnostico.form/diagnostico.form.component';
 import { LoginComponent } from './modulos/user/components/login/login.component';
+import { JwtGuard } from './guard/jwt.guard';
 
 const routes: Routes = [
-  { path : "login", component: LoginComponent },
-  { path : "paciente", component: PacienteListComponent },
-  { path : "paciente/view/:id", component: PacienteViewComponent },
-  { path : "paciente/new", component: PacienteFormComponent },
-  { path : "doctor", component: DoctorListaComponent },
-  { path : "doctor/view/:id", component: DoctorViewComponent },
-  { path : "doctor/new", component: DoctorFormComponent },
-  { path : "paciente/diagnostico/:id", component : DiagnosticoFormComponent },
-  { path : "" , redirectTo: '/login', pathMatch:'full'},
+  { path : "", component: LoginComponent },
+  { path : "paciente", component: PacienteListComponent ,canActivate:[JwtGuard]},
+  { path : "paciente/view/:id", component: PacienteViewComponent ,canActivate:[JwtGuard]},
+  { path : "paciente/new", component: PacienteFormComponent ,canActivate:[JwtGuard]},
+  { path : "doctor", component: DoctorListaComponent ,canActivate:[JwtGuard]},
+  { path : "doctor/view/:id", component: DoctorViewComponent ,canActivate:[JwtGuard]},
+  { path : "doctor/new", component: DoctorFormComponent,canActivate:[JwtGuard] },
+  { path : "paciente/diagnostico/:id", component : DiagnosticoFormComponent ,canActivate:[JwtGuard]},
+  { path : "" , redirectTo: '/', pathMatch:'full'},
   { path : "**", component: NotFoundComponent}
 ];
 
---
+++ src/app/guard/jwt.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from '../services/local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtGuard implements CanActivate {
+  constructor(private storage:LocalStorageService){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+
+    let token = this.storage.consultar('token');
+    if(token === ""){
+      return false;
+    }
+    return true;
+  }
+
+}
